31455d69e1e8fd3d0b1e36b7aa94de4b494b4b8f
Jeremy Hon <jeremy.hon.gy@gmail.com>
2017-09-30 22:57:26 -0700

    add chart and form skeletons

---
+++ src/Chart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Chart extends React.Component {
+  render() {
+    return (  
+      <div className="chart">
+        Chart goes here
+      </div>
+    )
+  }
+}
+
+Chart.propTypes = {
+};
+
+export default Chart;
---
+++ src/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="form">
+        Form goes here
+      </div>
+    )
+  }
+}
+
+Form.propTypes = {
+};
+
+export default Form;
