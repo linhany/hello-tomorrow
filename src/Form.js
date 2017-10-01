99427b7fa1b4fcb799f9ac2ccad576d0bcc74b33
Jeremy Hon <jeremy.hon.gy@gmail.com>
2017-09-30 23:15:31 -0700

    add react select

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import airports from './data/airports';
 
 class Form extends React.Component {
   render() {
     return (
       <div className="form">
-        Form goes here
+        Data Inputs
+        <div className="form-component">
+          <label>Origin</label>
+          <Select
+            name="origin"
+            options={airports}
+          />
+        </div>
       </div>
     )
   }
---
+++ src/data/airports.js
@@ -0,0 +1,6 @@
+const airports = [
+  {"value": "JFK", "label": "JFK"},
+  {"value": "SFO", "label": "SFO"}
+]
+
+export default airports
