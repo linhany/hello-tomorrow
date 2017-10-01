1976d9137567dc72159e7a747439c18ce60b7adb
John Yong <lhlhyong@gmail.com>
2017-10-01 01:09:52 -0700

    format abit

--- src/Chart.js
+++ src/Chart.js
@@ -2,27 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Bar} from 'react-chartjs-2';
 
-const xValues = ['950-1000',
-'900-950',
-'850-900',
-'800-850',
-'750-800',
-'700-750',
-'650-700',
-'600-650',
-'550-600',
-'500-550',
-'450-500',
-'400-450',
-'350-400',
-'300-350',
-'250-300',
-'200-250',
-'150-200',
-'100-150',
-'50-100',
-'0-50'].reverse();
-
+const xValues = ['950-1000', '900-950', '850-900', '800-850', '750-800', '700-750', '650-700',
+                 '600-650', '550-600', '500-550', '450-500', '400-450', '350-400', '300-350',
+                 '250-300', '200-250', '150-200', '100-150', '50-100', '0-50'].reverse();
 const yValues = [24, 12, 37, 69, 49, 49, 60, 77, 103, 132, 149, 158, 236, 226, 251, 293, 326, 217, 239, 67].reverse();
 
 const height = 150;
@@ -33,11 +15,11 @@ const highlightedCategory = '500-550';
 const normalBackgroundColor = 'rgba(255,99,132,0.2)';
 const normalHoverBackgroundColor = 'rgba(255,99,132,0.4)';
 
-const specialBackgroundColor = 'rgba(61, 118, 211, 0.4)';
-const specialHoverBackgroundColor = 'rgba(61, 118, 211, 0.8)';
+const specialBackgroundColor = 'rgba(61,118,211,0.4)';
+const specialHoverBackgroundColor = 'rgba(61,118,211,0.8)';
 
 const normalBorderColor = 'rgba(255,99,132,1)';
-const specialBorderColor = 'rgba(2, 53, 135, 1)'
+const specialBorderColor = 'rgba(2,53,135,1)'
 
 function generateBarChartColors(normal, special) {
   var resultArr = [];
